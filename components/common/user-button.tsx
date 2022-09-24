61ae52d220b67396b1c63cd209aef5f3ac486f80
Nauzet Hernandez <nauzethc@gmail.com>
2022-09-24 18:35:43 +0100

    Fix on avatar url

--- components/common/user-button.tsx
+++ components/common/user-button.tsx
@@ -2,6 +2,14 @@ import { Menu } from '@headlessui/react'
 import { LogoutIcon, UserCircleIcon } from '@heroicons/react/solid'
 import { useSession, signOut } from 'next-auth/react'
 
+function Avatar ({ src, alt }: { src?: string | null, alt?: string | null }) {
+  return src
+    ? <img className="w-8 h-8 rounded-full"
+      src={src}
+      alt={alt ?? 'User'} />
+    : <UserCircleIcon className="w-8 h-8" />
+}
+
 export default function UserButton ({ className = '' }: { className?: string }) {
   const { data: session } = useSession()
   const handleLogout = () => signOut()
@@ -9,12 +17,7 @@ export default function UserButton ({ className = '' }: { className?: string }) 
   return (session && session.user)
     ? <Menu as="div" className="dropdown h-10">
       <Menu.Button className="btn h-10 text-sm font-semibold px-1">
-        {(session.user.image || session.user.picture)
-          ? <img className="w-8 h-8 rounded-full"
-              src={session.user.image || session.user.picture}
-              alt={session.user.email ?? 'User'} />
-          : <UserCircleIcon className="w-8 h-8" />
-        }
+        <Avatar src={session.user.image || session.user.picture} alt={session.user.name} />
         <span className="hidden pr-2 sm:block">{session.user.name ?? session.user.email}</span>
       </Menu.Button>
       <Menu.Items className="dropdown-menu">
