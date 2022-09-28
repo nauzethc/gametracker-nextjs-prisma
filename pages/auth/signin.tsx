6b7573a9b3bee151fa60e1b9539f2b220c5a4cfb
Nauzet Hernández <nauzet@minos.local>
2022-09-28 17:35:51 +0100

    new: button for Twitch OAuth and error display based on errorCode

--- pages/auth/signin.tsx
+++ pages/auth/signin.tsx
@@ -2,9 +2,36 @@ import { PlayIcon } from '@heroicons/react/solid'
 import { signIn, useSession } from 'next-auth/react'
 import { useRouter } from 'next/router'
 import { useEffect } from 'react'
+import { toString } from '../../utils/url'
+
+function getErrorMessage (errorCode: string | undefined): string {
+  switch (errorCode) {
+    case 'OAuthSignin':
+      return 'Could not construct a valid request to this OAuth provider.'
+    case 'OAuthCallback':
+      return 'Could not handle response from OAuth provider.'
+    case 'OAuthCreateAccount':
+      return 'Could not create OAuth provider user.'
+    case 'OAuthAccountNotLinked':
+      return 'This account is already linked with another OAuth provider.'
+    default:
+      return errorCode ?? 'Unknown error'
+  }
+}
+
+function SignInError ({ error = '' }: { error: string | string[] | undefined }) {
+  const message = getErrorMessage(toString(error))
+  return message
+    ? <p className="error flex items-start gap-4 w-full">
+      <strong>Error</strong>
+      <span>{message}</span>
+    </p>
+    : null
+}
 
 export default function SignInView () {
   const router = useRouter()
+  const { error } = router.query
   const { data: session } = useSession()
 
   useEffect(() => {
@@ -13,24 +40,24 @@ export default function SignInView () {
 
   return (
     <div className="sign-in-view grid h-screen gap-12 place-content-center">
-      <div className="logo flex items-center text-3xl">
+      <div className="logo flex items-center justify-center text-3xl">
         <span className="font-light">Game</span>
         <span className="font-bold">Tracker</span>
         <PlayIcon className="w-10 h-10" />
       </div>
 
-      <div className="flex flex-col gap-2 w-full max-w-xs">
+      <div className="flex flex-col gap-2 w-full max-w-xs place-self-center">
         <button className="btn-github h-10" onClick={() => signIn('github')}>
           <svg viewBox="0 0 24 24" aria-hidden="true" className="h-6 w-6" fill="currentColor"><path fillRule="evenodd" clipRule="evenodd" d="M12 2C6.477 2 2 6.463 2 11.97c0 4.404 2.865 8.14 6.839 9.458.5.092.682-.216.682-.48 0-.236-.008-.864-.013-1.695-2.782.602-3.369-1.337-3.369-1.337-.454-1.151-1.11-1.458-1.11-1.458-.908-.618.069-.606.069-.606 1.003.07 1.531 1.027 1.531 1.027.892 1.524 2.341 1.084 2.91.828.092-.643.35-1.083.636-1.332-2.22-.251-4.555-1.107-4.555-4.927 0-1.088.39-1.979 1.029-2.675-.103-.252-.446-1.266.098-2.638 0 0 .84-.268 2.75 1.022A9.607 9.607 0 0 1 12 6.82c.85.004 1.705.114 2.504.336 1.909-1.29 2.747-1.022 2.747-1.022.546 1.372.202 2.386.1 2.638.64.696 1.028 1.587 1.028 2.675 0 3.83-2.339 4.673-4.566 4.92.359.307.678.915.678 1.846 0 1.332-.012 2.407-.012 2.734 0 .267.18.577.688.48 3.97-1.32 6.833-5.054 6.833-9.458C22 6.463 17.522 2 12 2Z"></path></svg>
           <span>Sign-in with Github</span>
         </button>
-        {/*
-        <button className="btn-google h-10" onClick={() => signIn('google')}>
-          <svg viewBox="0 0 30 30" aria-hidden="true" className="h-6 w-6" fill="currentColor"><path d="M 15.003906 3 C 8.3749062 3 3 8.373 3 15 C 3 21.627 8.3749062 27 15.003906 27 C 25.013906 27 27.269078 17.707 26.330078 13 L 25 13 L 22.732422 13 L 15 13 L 15 17 L 22.738281 17 C 21.848702 20.448251 18.725955 23 15 23 C 10.582 23 7 19.418 7 15 C 7 10.582 10.582 7 15 7 C 17.009 7 18.839141 7.74575 20.244141 8.96875 L 23.085938 6.1289062 C 20.951937 4.1849063 18.116906 3 15.003906 3 z"></path></svg>
-          <span>Sign-in with Google</span>
+        <button className="btn-twitch h-10" onClick={() => signIn('twitch')}>
+        <svg viewBox="0 0 500 500" aria-hidden="true" className="h-6 w-6" fill="currentColor"><g><path d="M329.3,384.8h-71.4l-47.6,47.7h-47.6v-47.7H75.5V131l23.7-63.5h325.2v222.2L329.3,384.8z M392.8,273.8V99.3H131v230.1h71.4v47.6l47.6-47.6h87.2L392.8,273.8z"/><path d="M305.5,162.8V258h31.8v-95.3H305.5z M218.2,257.9H250v-95.1h-31.8V257.9z"/></g></svg>
+          <span>Sign-in with Twitch</span>
         </button>
-        */}
       </div>
+
+      <SignInError error={error} />
     </div>
   )
 }
