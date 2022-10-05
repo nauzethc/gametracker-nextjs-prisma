180bef22cdd1c905d0f298a4bde798f2cf740825
Nauzet Hernandez <nauzet.hernandez@tng.iac.es>
2022-10-05 13:58:07 +0100

    component redesign

--- components/common/pagination.tsx
+++ components/common/pagination.tsx
@@ -15,6 +15,24 @@ export default function Pagination ({
 }: PaginationProps) {
   const numPages = Math.ceil(total / pageSize)
   const pages = [...new Array(numPages)].map((_, index) => index + 1)
+  const pagesToShow = [...new Set([
+    // First pages
+    ...pages.slice(0, 3),
+    // Middle pages
+    ...pages.slice(Math.max(0, page - 2), Math.min(page + 1, pages.length)),
+    // Last pages
+    ...pages.slice(pages.length - 3)
+  ])].sort((a, b) => a - b)
+
+  const pagination: (null|number)[] = []
+  // Include empty spaces between ranges
+  pagesToShow.forEach((page, index, pages) => {
+    pagination.push(page)
+    if (pages[index + 1] > page + 1) {
+      pagination.push(null)
+    }
+  })
+
   const handleChange = (page: number) => {
     if (typeof onChange === 'function') {
       onChange(page)
@@ -22,10 +40,13 @@ export default function Pagination ({
   }
   return (
     numPages > 1
-      ? <div className={`pagination flex items-center justify-center gap-2 ${className}`}>
-        {pages.map(numPage => numPage === page
-          ? <span className="current w-8 h-8 flex items-center justify-center rounded-lg" key={numPage}>{numPage}</span>
-          : <button onClick={() => handleChange(numPage)} className="w-8 h-8 rounded-lg" key={numPage}>{numPage}</button>
+      ? <div className={`pagination flex flex-wrap items-center justify-center gap-2 ${className}`}>
+        {pagination.map((numPage, index) =>
+          numPage === null
+            ? <span key={index}>...</span>
+            : numPage === page
+              ? <span className="current w-8 h-8 flex items-center justify-center rounded-lg" key={index}>{numPage}</span>
+              : <button onClick={() => handleChange(numPage)} className="w-8 h-8 rounded-lg" key={index}>{numPage}</button>
         )}
       </div>
       : null
