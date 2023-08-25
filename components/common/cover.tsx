f8c1f613cb28df3967427557ae19b744025f024b
Nauzet Hernández <nauzet@horizon.tng.iac.es>
2023-08-25 10:11:14 +0100

    Fix with new next/image component

--- components/common/cover.tsx
+++ components/common/cover.tsx
@@ -6,7 +6,7 @@ export default function Cover ({ className = '', src, alt } : { className?: stri
   return (
     <figure className={`cover ${className}`}>
       {src
-        ? <Image className="rounded" src={src} alt={alt} width={COVER[0]} height={COVER[1]} />
+        ? <Image className="rounded" src={src} alt={alt ?? 'Unknown'} width={COVER[0]} height={COVER[1]} />
         : <div className="placeholder rounded" style={{ width: COVER[0], maxWidth: '100%', aspectRatio: `${COVER[0] / COVER[1]}` }} />
       }
     </figure>
