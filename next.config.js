e638379a90558863bef350171a3ea2661e1cf138
Nauzet Hernández <nauzet@horizon.tng.iac.es>
2023-12-13 09:45:16 +0000

    Only cache static content and requested endpoints

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 const withPWA = require('next-pwa')({
   dest: 'public',
-  disable: process.env.NODE_ENV === 'development'
+  disable: process.env.NODE_ENV === 'development',
+  register: true,
+  skipWaiting: true,
+  scope: '/_next/'
 })
 
 /** @type {import('next').NextConfig} */
