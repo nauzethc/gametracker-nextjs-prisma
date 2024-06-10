319308837cdca2635f7c6f6b61be2b20a9f1342f
Nauzet Hernández <nauzet@minos.local>
2024-06-10 13:29:38 +0100

    string utilities extracted to tool library

--- utils/strings.ts
+++ utils/strings.ts
@@ -1,3 +1,13 @@
 export function capitalize (s: string): string {
   return s.substring(0, 1).toUpperCase() + s.substring(1)
 }
+
+export function toDateString (date?: string | Date | null): string {
+  return date !== null
+    ? (date instanceof Date ? date.toISOString() : date ?? '').slice(0, 10)
+    : ''
+}
+
+export function toHumanReadableString (date: Date | null): string {
+  return date ? new Date(date).toDateString() : 'Unknown release'
+}
