e092185b6fec03468c42a1edec13ef6913cd358f
Nauzet Hernández <nauzet@minos.local>
2024-06-10 13:15:34 +0100

    Components migrated to NextUI library

--- components/game-detail/game-preview.tsx
+++ components/game-detail/game-preview.tsx
@@ -1,13 +1,12 @@
-import Cover from '../common/cover'
 import { capitalize } from '../../utils/strings'
 import { GameWithPlatform } from '../../types/games'
 import { BookmarkIcon, CalendarIcon, ChartPieIcon, ClockIcon, RocketLaunchIcon, TrophyIcon } from '@heroicons/react/24/solid'
 import { BookmarkIcon as BookmarkOutlineIcon } from '@heroicons/react/24/outline'
 import { GamepadAltIcon, StatusIcon, GameplayIcon } from '../common/icons'
 import Rating from '../common/rating'
-import ProgressBar from '../common/progress-bar'
-import Link from 'next/link'
 import DateField from '../common/date-field'
+import { Button, Image, Link, Progress } from '@nextui-org/react'
+import { getStatusColor } from '../../utils/colors'
 
 type GamePreviewProps = {
   className?: string,
@@ -65,25 +64,29 @@ export default function GamePreview ({ className = '', data, onBookmark }: GameP
     <div className={`game-preview grid ${className}`}>
       <div className="flex flex-wrap gap-4 relative">
         <div className="w-32 flex-shrink-0">
-          <Cover src={data.cover || undefined} alt={data.name} />
+          <Image src={data.cover || undefined} alt={data.name} />
         </div>
         <div className="flex flex-col flex-grow gap-2">
           <h1 className="text-xl font-semibold flex items-center gap-2 justify-between">{data.name}
-          <button onClick={handleBookmark} className="btn-invisible h-10 w-10" aria-label="pinned">
+          <Button
+            isIconOnly
+            aria-label="pinned"
+            radius="full"
+            variant="light"
+            color="primary"
+            onClick={handleBookmark}>
             {data.fixed
-              ? <BookmarkIcon className="w-6 h-6" />
-              : <BookmarkOutlineIcon className="w-6 h-6" />
+              ? <BookmarkIcon className="size-6" />
+              : <BookmarkOutlineIcon className="size-6" />
             }
-          </button>
+          </Button>
         </h1>
           <div className="field flex flex-col">
             <span className="font-semibold text-sm">Developer</span>
             <ul className="comma-list">
               {data.developers.map((developer, index) =>
                 <li key={index}>
-                  <Link href={`/?developer=${developer}`} legacyBehavior>
-                    <a>{developer}</a>
-                  </Link>
+                  <Link href={`/?developer=${developer}`}>{developer}</Link>
                 </li>
               )}
             </ul>
@@ -93,9 +96,7 @@ export default function GamePreview ({ className = '', data, onBookmark }: GameP
             <ul className="comma-list">
               {data.publishers.map((publisher, index) =>
                 <li key={index}>
-                  <Link href={`/?publisher=${publisher}`} legacyBehavior>
-                    <a>{publisher}</a>
-                  </Link>
+                  <Link href={`/?publisher=${publisher}`}>{publisher}</Link>
                 </li>
               )}
             </ul>
@@ -109,55 +110,49 @@ export default function GamePreview ({ className = '', data, onBookmark }: GameP
       </div>
       <div className="grid gap-y-2 sm:grid-cols-2 py-6">
         <Stat label="Started on">
-          <CalendarIcon className="w-8 h-8" />
+          <CalendarIcon className="size-8" />
           <DateField className="text-lg"
             value={data.startedOn}
             format="medium" />
         </Stat>
         <Stat label="Status">
-          <StatusIcon status={data.status} className="w-8 h-8" />
+          <StatusIcon status={data.status} className="size-8" />
           <StatusField className="text-lg"
             status={data.status}
             date={data.finishedOn} />
         </Stat>
         <Stat label="Total hours">
-          <ClockIcon className="w-8 h-8" />
+          <ClockIcon className="size-8" />
           <span>{`${data.totalHours}h`}</span>
         </Stat>
         <Stat label="Progress">
-          <ChartPieIcon className="w-8 h-8" />
-          <div className="w-full max-w-xs">
-            <ProgressBar value={data.progress} className={data.status}>
-              <span className="text-sm font-semibold px-2">{data.progress}%</span>
-            </ProgressBar>
-          </div>
+          <ChartPieIcon className="size-8" />
+          <Progress value={data.progress} color={getStatusColor(data.status)} />
         </Stat>
         <Stat label="Gameplay">
-          <GameplayIcon gameplay={data.gameplayType} className="w-8 h-8" />
+          <GameplayIcon gameplay={data.gameplayType} className="size-8" />
           <span>{capitalize(data.gameplayType)}</span>
         </Stat>
         <Stat label="Platform">
-          <GamepadAltIcon className="w-8 h-8" />
-          <span>{data.platform.name}</span>
+          <GamepadAltIcon className="size-8" />
+          <Link href={`/?platformId=${data.platformId}`}>{data.platform.name}</Link>
         </Stat>
         <Stat label="Achievements">
-          <TrophyIcon className="w-8 h-8" />
+          <TrophyIcon className="size-8" />
           <span>{`${data.achievementsUnlocked} of ${data.achievementsTotal}`}</span>
         </Stat>
         <Stat label="Genres">
-          <RocketLaunchIcon className="w-8 h-8" />
+          <RocketLaunchIcon className="size-8" />
           <ul className="comma-list">
             {data.genres.map((genre, index) =>
               <li key={index}>
-                <Link href={`/?genre=${genre}`} legacyBehavior>
-                  <a>{genre}</a>
-                </Link>
+                <Link href={`/?genre=${genre}`}>{genre}</Link>
               </li>
             )}
           </ul>
         </Stat>
         <div className="col-span-full flex flex-col gap-4 items-center justify-center mt-4">
-          <Rating className="text-yellow-400 dark:text-yellow-500" max={5} value={Number(data.rating)} size={8} />
+          <Rating max={5} value={Number(data.rating)} size={8} />
           {data.comment ? <blockquote className="px-4">{data.comment}</blockquote> : null}
         </div>
       </div>
