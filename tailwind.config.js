9004252cdaeebbad8cc03b1341646a82bab81834
Nauzet Hernández <nauzet@minos.local>
2024-06-10 13:27:16 +0100

    migrated to NextUI components and utilities

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,12 @@
 /** @type {import('tailwindcss').Config} */
+const { nextui } = require('@nextui-org/react')
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}'
+    './components/**/*.{js,ts,jsx,tsx}',
+    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
     extend: {
@@ -13,7 +15,9 @@ module.exports = {
       }
     }
   },
+  darkMode: 'class',
   plugins: [
-    require('@tailwindcss/forms')
+    nextui(),
+    require('@tailwindcss/container-queries')
   ]
 }
