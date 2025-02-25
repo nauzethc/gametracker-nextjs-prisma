6050e8e71ebaa40e0ab78caa0bf447c8f225f8e5
Nauzet Hernandez <nauzethc@gmail.com>
2025-02-25 14:08:21 +0000

    fix: mask was not applied when many rating components were rendered on under same parent

--- components/common/rating.tsx
+++ components/common/rating.tsx
@@ -1,4 +1,5 @@
 import { StarIcon } from '@heroicons/react/24/solid'
+import { useId } from 'react'
 
 export default function Rating ({
   className = '',
@@ -11,6 +12,7 @@ export default function Rating ({
   max?: number,
   size?: number
 }) {
+  const maskId = useId()
   const fillStars = Math.floor(value)
   const emptyStars = Math.floor(max - value)
   const hasHalf = Math.ceil(max - value) - Math.floor(max - value)
@@ -18,8 +20,8 @@ export default function Rating ({
     <div className={`rating flex items-center ${className}`}>
       {[...new Array(fillStars)].map((_, index) => <StarIcon className={`size-${size}`} key={index} />)}
       {hasHalf
-        ? <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className={`size-${size} mask-w-50`}>
-            <mask id="halfMask">
+        ? <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className={`size-${size}`}>
+            <mask id={maskId}>
               <polygon fill="#ffffff" points="0,0 0,24 12,24 12,0"></polygon>
               <polygon fill="rgba(255,255,255,0.3)" points="12,0 12,24 24,24 24,0"></polygon>
             </mask>
@@ -27,7 +29,7 @@ export default function Rating ({
               fillRule="evenodd"
               d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
               clipRule="evenodd"
-              mask="url(#halfMask)" />
+              mask={`url(#${maskId})`} />
           </svg>
         : null
       }
